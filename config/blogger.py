d8686f41a56805c7bc0d934a53c69989b12ed4a0
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-12-05 11:20:52 +0530

    format: use black

--- config/blogger.py
+++ config/blogger.py
@@ -1,16 +1,20 @@
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
-from config.dependencies import get_db,  get_current_user
+from config.dependencies import get_db, get_current_user
 from models.models import User
 from models.models import Post
 
 
-def get_post_for_user(post_id:str,  db:Session= Depends(get_db), user: User=Depends(get_current_user)) -> Post:
+def get_post_for_user(
+    post_id: str, db: Session = Depends(get_db), user: User = Depends(get_current_user)
+) -> Post:
     post = db.query(Post).filter(Post.id == post_id).first()
     if not post:
-        raise HTTPException(status_code = 404, detail="Post not found")
-    
+        raise HTTPException(status_code=404, detail="Post not found")
+
     if post.author.id != user.id:
-        raise HTTPException(status_code = 403, detail="You don't have permissions to modify this post")
-    
+        raise HTTPException(
+            status_code=403, detail="You don't have permissions to modify this post"
+        )
+
     return post
--- config/dependencies.py
+++ config/dependencies.py
@@ -18,23 +18,26 @@ def get_db():
         db.close()
 
 
-
-def get_current_user(token:str = Depends(JWTBearer())) -> User:
+def get_current_user(token: str = Depends(JWTBearer())) -> User:
     try:
-        payload = jwt.decode(token, f'{settings.SECRET_KEY}', algorithms=['HS256'])
-        user_id = payload.get('sub')
+        payload = jwt.decode(token, f"{settings.SECRET_KEY}", algorithms=["HS256"])
+        user_id = payload.get("sub")
         db = SessionLocal()
         return db.query(User).filter(User.email == user_id).first()
-    except(jwt.PyJWTError, AttributeError):
-        return  HTTPException(status_code="Invalid token")
+    except (jwt.PyJWTError, AttributeError):
+        return HTTPException(status_code="Invalid token")
 
 
-def get_post_for_user(post_id:str,  db:Session= Depends(get_db), user: User=Depends(get_current_user)) -> Post:
+def get_post_for_user(
+    post_id: str, db: Session = Depends(get_db), user: User = Depends(get_current_user)
+) -> Post:
     post = db.query(Post).filter(Post.id == post_id).first()
     if not post:
-        raise HTTPException(status_code = 404, detail="Post not found")
-    
+        raise HTTPException(status_code=404, detail="Post not found")
+
     if post.author.id != user.id:
-        raise HTTPException(status_code = 403, detail="You don't have permissions to modify this post")
-    
+        raise HTTPException(
+            status_code=403, detail="You don't have permissions to modify this post"
+        )
+
     return post
